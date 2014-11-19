85fe1e1ff92e182422c66762886ec4bc84b81216
Vadim Rastyagaev <rastyagaev@gmail.com>
2014-11-19 17:52:29 +0300

    added capistrano-secrets

--- Capfile
+++ Capfile
@@ -7,7 +7,7 @@ require 'capistrano/deploy'
 require 'capistrano/bundler'
 require 'capistrano/bower'
 require 'capistrano/rails'
-# require 'capistrano/secrets'
+require 'capistrano/secrets'
 require 'capistrano/services'
 
 # Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
--- config/deploy.rb
+++ config/deploy.rb
@@ -8,8 +8,8 @@ set :deploy_to, "/home/#{fetch(:user)}/applications/#{fetch(:application)}"
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bower_components}
 set :linked_files, %w{.env}
 set :ssh_options, { forward_agent: true }
-# set :secrets_app_name, 'f-s-v-secrets'
-# set :secrets_app_pattern, 'O2S'
+set :secrets_app_name, 'f-s-v-secrets'
+set :secrets_app_pattern, 'O2S'
 set :services, [
   {name: fetch(:application), roles: :app, sudo: true}
 ]
